2aa15c27b1ea2f447f2808817bf5aa7cc85ac735
imnaimur <naimur58co@gmail.com>
2024-10-13 12:30:12 +0600

    django starter

---
+++ a_users/signals.py
@@ -0,0 +1,13 @@
+from .models import Profile
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+@receiver(post_save,sender = User)
+def user_postsave(sender,instance,created,**kwargs):
+    user = instance
+
+    if created:
+        Profile.objects.create(
+            user = user,
+        )
